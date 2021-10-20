2047bfde31bb4e601aae3ed225ccd824cb2387a4
MS <72651078+madblocksgit@users.noreply.github.com>
2021-10-20 13:04:54 +0530

    Create beepBuzzer.ino

---
+++ Buzzer_Example/beepBuzzer.ino
@@ -0,0 +1,22 @@
+/****************************************************************************
+Agenda      :   Beeping the on-board Buzzer
+Hardware    :   madBlocks IoT Suite
+Programmer  :   Maddy
+Date        :   20th Oct 2021
+*****************************************************************************/
+
+int buzzer=4;
+
+#define BUZZER_ON 1
+#define BUZZER_OFF 0
+
+void setup() {
+ pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+ digitalWrite(buzzer,BUZZER_ON);
+ delay(100);
+ digitalWrite(buzzer,BUZZER_OFF);
+ delay(100);
+}
