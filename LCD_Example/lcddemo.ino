203365c51f046f9ef0977dd040691624bce42e53
MS <72651078+madblocksgit@users.noreply.github.com>
2021-10-20 13:44:14 +0530

    Create lcddemo.ino

---
+++ LCD_Example/lcddemo.ino
@@ -0,0 +1,28 @@
+/****************************************************************************
+Agenda      :   LCD Demo
+Hardware    :   madBlocks IoT Suite
+Programmer  :   Maddy
+Date        :   20th Oct 2021
+*****************************************************************************/
+
+#include <LiquidCrystal.h>
+
+int LCD_RS=23;
+int LCD_EN=15;
+int LCD_D4=25;
+int LCD_D5=26;
+int LCD_D6=27;
+int LCD_D7=14;
+
+LiquidCrystal lcd(LCD_RS,LCD_EN,LCD_D4,LCD_D5,LCD_D6,LCD_D7);
+
+void setup() {
+  lcd.begin(16,2);
+  lcd.print("*** WELCOME TO");
+  lcd.setCursor(0,1);
+  lcd.print("   IoT Suite ***");
+}
+
+void loop() {
+  
+}
